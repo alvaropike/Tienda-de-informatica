09a53fef3ae08aa950b699d989ff9bc02ba74dc5
alvaropike <alvaropiqueras@hotmail.com>
2020-02-06 19:18:31 +0100

    Add files via upload

--- Producto.php
+++ Producto.php
@@ -11,12 +11,13 @@
     require_once $_SERVER['DOCUMENT_ROOT']."/AppWeb/tiendaInformatica/Tienda-de-informatica/dirs.php";
     require_once CONTROLLER_PATH."ControladorAcceso.php";
     require_once CONTROLLER_PATH."ControladorUsuario.php";
+    require_once VIEW_PATH."cabecera.php"; 
 
 
-    if ( ($_SESSION['admin'])=="si"){
-        require_once VIEW_PATH2."principal.php";
+    if ((($_SESSION['admin'])=="si") && (isset($_SESSION['USUARIO']['email']))){
+        require_once VIEW_PATH2."admin_listado.php";
 
-    }else if ( ($_SESSION['admin'])=="no"){
+    }else{
         header("location: vistas/error.php");
     }
     
--- Usuario.php
+++ Usuario.php
@@ -6,18 +6,21 @@
     //echo date("d/m/Y", time());
     //echo ROOT_PATH;
     //exit();
+    
     error_reporting(E_ALL & ~(E_STRICT|E_NOTICE));
     session_start();
 
     require_once $_SERVER['DOCUMENT_ROOT']."/AppWeb/tiendaInformatica/Tienda-de-informatica/dirs.php";
     require_once CONTROLLER_PATH."ControladorAcceso.php";
     require_once CONTROLLER_PATH."ControladorUsuario.php";
+    require_once VIEW_PATH."cabecera.php"; 
 
 
-    if ( ($_SESSION['admin'])=="si"){
-        require_once VIEW_PATH."principal.php";
+    if ((($_SESSION['admin'])=="si") && (isset($_SESSION['USUARIO']['email']))){
+        // require_once VIEW_PATH."principal.php";
+        require_once VIEW_PATH."admin_listado.php";
 
-    }else if ( ($_SESSION['admin'])=="no"){
+    }else{
         header("location: vistas/error.php");
     }
     
